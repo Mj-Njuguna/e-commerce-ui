0d17a74d5d3c1a0439deaaf0b0f153247d2776dc
Mj-Njuguna <jessemike0002@gmail.com>
2026-05-25 03:49:26 +0300

    Categories-and-ProductList-components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import ProductList from "@/components/ProductList";
 import Image from "next/image";
 const Homepage = () => {
   return (
     <div className="">
-      <div className="relative aspect-[]">
-        <Image src="/featured.png" alt="Featured Product" />
+      <div className="relative aspect-[3/1] mb-12">
+        <Image src="/featured.png" alt="Featured Product" fill />
       </div>
+      <ProductList />
     </div>
   );
 };
